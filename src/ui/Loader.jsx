bad3a49bb3f14679625622380d8d0f777a96576d
Moeez Nadeem <moeeznadeem143@gmail.com>
2024-06-27 00:19:44 +0500

    first time

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="absolute bg-slate-200/30 inset-0 drop-shadow-sm flex items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
